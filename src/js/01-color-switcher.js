87142bd836241aebefe8350bf28a748bf04f9d3e
ZemskovAlexander <zemskov25121992@gmail.com>
2022-08-29 22:00:21 +0300

    upd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//   }
 
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+// const body = document.querySelector('body');
+
+let timerId = null;
+
+start.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+  }, 1000);
+});
+
+
+stop.addEventListener("click", () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
