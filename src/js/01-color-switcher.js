aa3689d49c087a61de6acb60a00f70ea6497b971
ZemskovAlexander <zemskov25121992@gmail.com>
2022-09-01 20:33:48 +0300

    добавил проверку кнопки. done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,10 +7,20 @@ const stop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 
 let timerId = null;
+let isActive = false;
 
 start.addEventListener('click', () => {
+  if (this.isActive) {
+    return;
+  }
+
+  this.isActive = true;
+
   timerId = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor()}, 1000);
+    console.log(`Interval with id ${timerId} has run!`);
+
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 });
 
 stop.addEventListener('click', () => {
