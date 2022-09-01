007221cfece35bcd46a7ecf54684f24f283fbba3
ZemskovAlexander <98232717+ZemskovAlexander@users.noreply.github.com>
2022-09-01 16:43:49 +0000

    Deploying to gh-pages from @ ZemskovAlexander/goit-js-hw-09@086b1a31338337daf7b5c899ab84951f6b4eba0a 🚀

---
+++ 01-color-switcher.e8a588b9.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),console.log("Interval with id ".concat(o," has stopped!"))}))}();
+//# sourceMappingURL=01-color-switcher.e8a588b9.js.map
