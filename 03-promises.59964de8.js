d63b0c88534cc195c7cd3ec09979d8b377ab5f50
ZemskovAlexander <98232717+ZemskovAlexander@users.noreply.github.com>
2022-09-08 10:16:40 +0000

    Deploying to gh-pages from @ ZemskovAlexander/goit-js-hw-09@c512704883e26951bd36f5ba6208eb75469be96b 🚀

---
+++ 03-promises.59964de8.js
@@ -0,0 +1,2 @@
+!function(){var n=document.querySelector("button[submit]");console.log(n),n.addEventListener("submit",(function(t){t.preventDefault(),new FormData(n).forEach((function(n,t){return console.log(n,t)}))}))}();
+//# sourceMappingURL=03-promises.59964de8.js.map
