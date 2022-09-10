b4087ae4272dba596b68f879bd0071330e732240
ZemskovAlexander <zemskov25121992@gmail.com>
2022-09-10 21:32:51 +0300

    add promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,13 +4,22 @@ inputData.addEventListener('submit', evt => {
   evt.preventDefault();
   const formData = new FormData(inputData);
   formData.forEach((value, name) => console.log(value, name));
+  createPromise();
 });
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+   console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
