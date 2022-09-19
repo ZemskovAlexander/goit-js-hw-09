efa48958bc4eb5b91f32725e9655219c546752fe
ZemskovAlexander <98232717+ZemskovAlexander@users.noreply.github.com>
2022-09-19 18:50:56 +0000

    Deploying to gh-pages from @ ZemskovAlexander/goit-js-hw-09@e985f07b72974f7ccc3874c799cd638059514346 🚀

--- 03-promises.e9197eb1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form");e.addEventListener("submit",(o=>{o.preventDefault();return new FormData(e).forEach(((e,o)=>console.log(e,o))),new Promise(reject.then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)})))}));
-//# sourceMappingURL=03-promises.e9197eb1.js.map
